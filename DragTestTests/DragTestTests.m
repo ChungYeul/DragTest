dada0d5d363f4531649b3022b22d55d5c0922331
iasandcb <iasandcb@gmail.com>
2014-01-16 15:34:02 +0900

    Initial Commit

---
+++ DragTestTests/DragTestTests.m
@@ -0,0 +1,34 @@
+//
+//  DragTestTests.m
+//  DragTestTests
+//
+//  Created by T on 2014. 1. 16..
+//  Copyright (c) 2014년 T. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DragTestTests : XCTestCase
+
+@end
+
+@implementation DragTestTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
