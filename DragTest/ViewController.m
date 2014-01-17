6ea77b07a19633afc96f0efe172253770f3f00b8
iasandcb <iasandcb@gmail.com>
2014-01-17 18:08:37 +0900

    뷰를 드래그앤드롭한다

--- DragTest/ViewController.m
+++ DragTest/ViewController.m
@@ -9,15 +9,33 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIView *dragView;
 
 @end
 
 @implementation ViewController
+- (IBAction)moveView:(UIPanGestureRecognizer *)sender {
+    // dragView의 좌표
+    CGPoint touchPoint = [sender locationInView:self.view];
+    if (sender.state == UIGestureRecognizerStateBegan) {
+    [UIView beginAnimations:nil context:nil];
+    self.dragView.center = touchPoint;
+    [UIView commitAnimations];
+    } else {
+    self.dragView.center = touchPoint;
+    }
+//    NSLog(@"M");
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    self.dragView.center = CGPointMake(0, 0);
 }
 
 - (void)didReceiveMemoryWarning
